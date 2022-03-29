f8e29e27620fcf0aedb48559b5c014d75f622324
Munasar abuukar <abdihalim415@gmail.com>
2022-03-30 01:35:32 +0300

    edited error zone

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -12,7 +12,9 @@ const Error: NextPage<ErrorProps> = ({ statusCode }) => {
       <title>Page kan maaha mid jira</title>
         <Header />
         <div className="flex justify-between p-5 max-w-7xl mx-auto">
-            <h1>The page is : {statusCode} go back to home please</h1>
+        <h1 className='text-6xl max-w-xl font-serif'>
+                <span className='underline decoration-black decoration-4'>The page is :</span> {""}  i{statusCode} go back to home please
+              </h1>
         </div>
       
     </div>

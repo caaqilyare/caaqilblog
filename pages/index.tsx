b2c06f053290c23ce5c80dd8134151d64c267e3a
Munasar abuukar <abdihalim415@gmail.com>
2022-03-30 01:20:45 +0300

    added some text

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,9 +21,9 @@ export default function  Home ({ posts }: Props) {
         <div className='flex justify-between items-center bg-yellow-400 border-y border-black py-10 lg:py-0'>
           <div className='px-10 space-y-5'> 
               <h1 className='text-6xl max-w-xl font-serif'>
-                <span className='underline decoration-black decoration-4'>Medium</span> {""} is place to write , read , and connect
+                <span className='underline decoration-black decoration-4'>Medium</span> {""} waa meesha wax lagu qoro , wax lagu aqriyo , laguna xiriiro
               </h1>
-              <h2>It's easy and free to post your thinkging on any topic and connect with millions of readers.
+              <h2>Project gaan waxaan ku sameeyay Next js front end  iyo Sanity back end Waana simple website 
               </h2>
           </div>
             <img  className='hidden md:inline-flex h-32 lg:h-full'
